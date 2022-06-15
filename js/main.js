6752cab69e37e1a062017a438533f29de96ff2bd
AmonovSokhib <amonovsokhib@gmail.com>
2022-06-15 17:29:54 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+
+var elForm = document.querySelector(".js-form")
+var elInput = document.querySelector(".js-input")
+var elResult = document.querySelector(".js-result")
+var elSellect = document.querySelector(".js-sellect")
+    var doler = 11000;
+    var evro = 11800;
+    var rubl = 190;
+
+        elForm.addEventListener("submit", function(evt){
+            evt.preventDefault();
+            var inputVal = +elInput.value;
+            var sellectVal = elSellect.value;
+            if(sellectVal = "USD"){
+                elResult.textContent = inputVal / doler;
+                
+            }
+            if(sellectVal = "RUB"){
+                elResult.textContent = inputVal / rubl;
+                console.log(inputVal / rubl);
+            }
+            if(sellectVal = "EVRO"){
+                elResult.textContent = inputVal / evro;
+                
+            }
+            else {
+                elResult.textContent = "dddd"; 
+            }
+
+        })
+
